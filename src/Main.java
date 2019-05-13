491f9d6c70ad8321c9eadf36c975fc6e88f48e2c
pawelmisiak <thepawelmisiak@gmail.com>
2019-05-13 19:25:02 -0400

    working on filling the array

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import java.lang.reflect.Array;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World! " + args[0]);
+//        System.out.println("Hello World! " + args[0]);
+
+        int upperLimit = 4999;
+        int lowerLimit = 0;
+        int[] cylinders = new int[1000];
+
+        fillArray(cylinders);
+
+
+//        System.out.println(lowerLimit);
     }
+
+    private static void fillArray(int [] arr){
+
+        
+        System.out.println(arr[0]);
+    }
+
+
 }
