2591768b8bed85fdff06505c5e1f197c3ddba1b5
pawelmisiak <thepawelmisiak@gmail.com>
2019-05-13 21:03:26 -0400

    finish working on ssft, since Jose is working on scan I move on to look alg.

--- src/Main.java
+++ src/Main.java
@@ -18,14 +18,32 @@ public class Main {
 
 
         // Testing //
-        fcfs(2, test);
+//        fcfs(2, test);
+//        sstf(2, test);
 
+//        System.out.println(getclosestCylinder(16, test));
+        System.out.println("the result is " + sstf(2, test));
 
         // Ready Algorithms //
 //        fcfs(Integer.parseInt(args[0]), cylinders);
+//        sstf(Integer.parseInt(args[0]), test);
     }
 
 
+    private static int sstf(int head, int [] arr){
+        int sum = 0;
+        int currentCylinder = head;
+
+        for(int i = 0; i < arr.length; i ++){
+            int closest = getclosestCylinder(currentCylinder, arr);
+            System.out.println("closest: " + arr[closest]);
+            sum += getDistance(currentCylinder,arr[closest]);
+            currentCylinder = arr[i];
+            arr[closest] = 9999;
+        }
+        System.out.println(sum);
+        return sum;
+    }
 
     private static int fcfs(int head, int [] arr){
         int sum = Math.abs(head - arr[0]);
@@ -41,6 +59,25 @@ public class Main {
         for(int i = 0; i < arr.length; i++){
             arr[i] = (int)(Math.random()*4999);
         }
+    }
+
+    private static int getclosestCylinder(int beggining, int [] arr){
+        int closest = 4999;
+        int cylinderLocation = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 9999){
+                int currentDistance = getDistance(beggining, arr[i]);
+                if(currentDistance < closest){
+
+                    closest = currentDistance;
+
+                    cylinderLocation = i;
+                }
+            }
+        }
+
+        return cylinderLocation;
     }
 
     private static int getDistance(int a, int b){
